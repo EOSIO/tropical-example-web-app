787f1d9e1f04616852e299a5372c08a2cedb0166
Bart Wyatt <bart.wyatt@block.one>
2019-09-18 11:52:04 -0400

    refactored enrollment into util file, fixed issues with API and eos-js versioning

--- src/App.js
+++ src/App.js
@@ -3,12 +3,13 @@ import { func, shape, instanceOf } from 'prop-types'
 // HOC (Higher Order Component) to pass the UALProvider context to the component
 import { withUAL } from 'ual-reactjs-renderer'
 import 'App.scss'
-import base64url from 'base64url'
 
 import NavigationBar from 'components/navigation/NavigationBar'
 import NotificationBar from 'components/notification/NotificationBar'
 import ResultsPage from 'components/results/ResultsPage'
 import LandingPage from 'components/landing/LandingPage'
+
+import {generateWebauthnPubkey, enrollWebauthnPubkey} from "utils/webauthn"
 
 class App extends React.Component {
   static propTypes = {
@@ -77,84 +78,14 @@ class App extends React.Component {
     if (activeUser) {
       try {
         const accountName = await activeUser.getAccountName()
-        const createCredentialOptions = {
-          // Format of new credentials is publicKey
-          publicKey: {
-            // Relying Party
-            rp: {
-              name: "Tropical Stay",
-              id: "localhost"
-            },
-            // Cryptographic challenge from the server
-            challenge: new Uint8Array(26),
-            // User
-            user: {
-              id: new Uint8Array(16),
-              name: accountName,
-              displayName: accountName,
-            },
-            // Requested format of new keypair
-            pubKeyCredParams: [{
-              type: "public-key",
-              alg: -7,
-            }],
-            timeout: 60000,
-            attestation: 'direct'
-          }
-        }
-
-        const webauthnResp = await navigator.credentials.create(createCredentialOptions)
-        const publicKeyCredentialToJSON = (pubKeyCred) => {
-          if(pubKeyCred instanceof Array) {
-            let arr = [];
-            for(let i of pubKeyCred)
-              arr.push(publicKeyCredentialToJSON(i));
-
-            return arr
-          }
-
-          if(pubKeyCred instanceof ArrayBuffer) {
-            return base64url.encode(pubKeyCred)
-          }
-
-          if(pubKeyCred instanceof Object) {
-            let obj = {};
-
-            for (let key in pubKeyCred) {
-              obj[key] = publicKeyCredentialToJSON(pubKeyCred[key])
-            }
-
-            return obj
-          }
-
-          return pubKeyCred
-        }
-
-        const payload = {
-          name: accountName,
-          webauthnResp: publicKeyCredentialToJSON(webauthnResp)
-        }
-
-        const enrollResponse = await fetch('/api/enroll', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(payload)
-        })
-
-        const enrollResult = await enrollResponse.json()
-        if (!enrollResult.status || enrollResult.status !== "ok")  {
-          this.displayError({message: "Enrollment failed"});
-        } else {
-          onSuccess()
-        }
+        const pubkey = await generateWebauthnPubkey(accountName)
+        await enrollWebauthnPubkey(accountName, pubkey)
+        onSuccess();
       } catch (err) {
-        console.error('Enroll Error', JSON.parse(JSON.stringify(err)))
         this.displayError(err)
       }
     } else {
-      this.displayError({message: "Not Logged In!"})
+      this.displayError(new Error("Not Logged In!"))
     }
   }
 
