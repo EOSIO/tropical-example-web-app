18d679d6810c38e7a437c204e7d605619f856f51
Mike Manfredi <mike.manfredi@block.one>
2019-09-20 15:51:52 -0400

    removed unused code

--- src/components/results/property/PropertyImage.js
+++ src/components/results/property/PropertyImage.js
@@ -2,17 +2,11 @@ import React from 'react'
 import { bool } from 'prop-types'
 import './PropertyImage.scss'
 
-import heart from 'assets/images/heart/heart.svg'
-import likedHeart from 'assets/images/heart/liked-heart.svg'
-import loadingHeart from 'assets/images/heart/loading-heart.svg'
 import propertySvg from 'assets/images/property.svg'
 
-const PropertyImage = ({ loading, liked }) => (
+const PropertyImage = () => (
   <div className='property-image-container'>
     <img src={propertySvg} alt='property' />
-    {/* <img className={`heart ${!liked && !loading ? 'display' : ''}`} src={heart} alt='heart' />
-    <img className={`heart ${liked && !loading ? 'display' : ''}`} src={likedHeart} alt='liked-heart' />
-    <img className={`heart ${loading ? 'display' : ''}`} src={loadingHeart} alt='loading-heart' /> */}
   </div>
 )
 
