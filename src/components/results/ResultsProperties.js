b190d4426d2c2ce155347d324b985467ae7cb6a1
Mike Manfredi <mike.manfredi@block.one>
2019-10-15 17:55:29 -0400

    adding enrollment check before renting

--- src/components/results/ResultsProperties.js
+++ src/components/results/ResultsProperties.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { func } from 'prop-types'
+import { func, bool } from 'prop-types'
 import './ResultsProperties.scss'
 
 import Property from 'components/results/property/Property'
 
 const NUM_PROPERTIES = 8
 
-const ResultsProperties = ({ login, displayError }) => (
+const ResultsProperties = ({ login, displayError, enrolled }) => (
   <div className='results-properties-container'>
-    { [...Array(NUM_PROPERTIES).keys()].map(e => <Property login={login} displayError={displayError} key={e} />)
+    { [...Array(NUM_PROPERTIES).keys()].map(e => <Property login={login} displayError={displayError} key={e} enrolled={enrolled} />)
     }
   </div>
 )
@@ -16,6 +16,7 @@ const ResultsProperties = ({ login, displayError }) => (
 ResultsProperties.propTypes = {
   login: func.isRequired,
   displayError: func.isRequired,
+  enrolled: bool.isRequired,
 }
 
 export default ResultsProperties
