6ef1e86f53fea8a14c8670953181d8c9793588b9
Mike Manfredi <mike.manfredi@block.one>
2019-10-22 14:00:35 -0400

    adding check to ensure .env exists

---
+++ scripts/copyDefaultEnv.js
@@ -0,0 +1,8 @@
+var fs = require('fs');
+var path = require('path');
+var root = __dirname.replace('scripts', '');
+
+if (!fs.existsSync(path.join(root + '.env'))) {
+  fs.copyFileSync(path.join(root + 'default.env'), path.join(root + '.env'));
+  console.info('.env NOT found; copying ./default.env to ./.env');
+}
